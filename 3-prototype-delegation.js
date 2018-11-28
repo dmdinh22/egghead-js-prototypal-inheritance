5f25cef98aeb44fafe154881ea1eefa765f58b0e
David Dinh <dmdinh22@gmail.com>
2018-11-27 22:15:57 -0800

    prototypal delegation

--- 3-prototype-delegation.js
+++ 3-prototype-delegation.js
@@ -3,6 +3,12 @@ function Car(make) {
     this.wheels = 1;
 }
 
+Car.prototype.color = 'black';
+
+Car.prototype.wheels = 4;
+
+console.log(Car.prototype); // Car { color: 'black', wheels: 4 }
+
 const myCar = new Car('Ford');
 
-console.log(myCar); // Car { make: 'Ford', wheels: 1 }
+console.log(myCar.color); // black
